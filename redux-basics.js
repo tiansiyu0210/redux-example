10e7ba54e80fceb5ed26a2283f23b2455c7357fd
tiansiyu0210 <tiansiyu297@gmail.com>
2019-10-01 21:28:21 -0400

    283. Setting Up Reducer and Store 284. Dispatching Actions 285. Adding Subscriptions

---
+++ redux-basics.js
@@ -0,0 +1,42 @@
+//import {} from ;
+//run node redux-basics.js
+const redux = require('redux');
+const createStore = redux.createStore;
+
+const initialState = {
+    counter: 0
+}
+
+// Reducer
+// need new state as argument, and return updated state
+const rootReducer = (state = initialState, action) => {
+    if(action.type === 'INC_COUNTER'){
+        return {
+            ...state,
+            counter: state.counter + 1
+        }
+    }
+
+    if(action.type === 'ADD_COUNTER'){
+        return {
+            ...state,
+            counter: state.counter + action.value
+        }
+    }
+    return state;
+}
+
+// Store
+const store = createStore(rootReducer);
+console.log(store.getState());
+
+// Subscription
+store.subscribe(() => {
+    console.log('[Subscription]', store.getState());
+})
+
+// Dispatching Action
+store.dispatch({type: 'INC_COUNTER'});//increase by 1
+store.dispatch({type: 'ADD_COUNTER', value: 10});//add by cretan number
+console.log(store.getState());
+
